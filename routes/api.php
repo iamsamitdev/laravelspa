c176f15a419d63f389d616a3b571c8d972a2da50
Samit Koyom <samitkoyom@gmail.com>
2020-06-04 00:27:37 +0700

    day9 complete

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\DB;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +18,30 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('products', function () {
+    $product_data = DB::table('products')->get();
+    dd($product_data);
+    return true;
+});
+
+
+Route::post('products', function (Request $request) {
+
+    $product_data =array(
+        'product_name' => $request->product_name,
+        'product_detail' => $request->product_detail,
+        'product_barcode' => $request->product_barcode,
+        'product_qty' => $request->product_qty,
+        'product_price' => $request->product_price,
+        'product_image' => $request->product_image,
+        'product_category' => $request->product_category,
+        'product_status' => $request->product_status,
+        'created_at' => NOW()
+    );
+
+    $result = DB::table('products')->insert($product_data);
+    return $result;
+
+});
+
